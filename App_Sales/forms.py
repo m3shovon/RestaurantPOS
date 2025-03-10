49ed3deb1c0da8c3cdf004e48fb4826bb2536716
Mufrid Shovon <mia.md.mufrid@gmail.com>
2025-03-10 11:24:57 +0600

    simple changes

--- App_Sales/forms.py
+++ App_Sales/forms.py
@@ -7,6 +7,16 @@ class InvoiceForm(forms.ModelForm):
         model = Invoice
         fields = ['payment_method', 'payment_amount', 'discount']
 
+# class InvoiceItemForm(forms.ModelForm):
+#     class Meta:
+#         model = InvoiceItem
+#         fields = ['item', 'quantity', 'price']
+    
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.fields['item'].queryset = Items.objects.all()  
+
+
 class InvoiceItemForm(forms.ModelForm):
     class Meta:
         model = InvoiceItem
@@ -14,4 +24,11 @@ class InvoiceItemForm(forms.ModelForm):
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['item'].queryset = Items.objects.all()  
+        self.fields['item'].queryset = Items.objects.all()
+        self.fields['item'].widget.attrs.update({'onchange': 'updatePrice(this)'})
+        
+        # Add price as data attribute in options
+        self.fields['item'].widget.choices = [
+            (item.id, f"{item.title} - ${item.selling_price}") for item in Items.objects.all()
+        ]
+        
